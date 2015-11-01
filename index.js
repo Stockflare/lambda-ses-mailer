f588c22408905eef372ec3fa5bbb2ecd6386b9b3
davidkelley <david.james.kelley@gmail.com>
2015-11-01 17:24:52 +0000

    modified code to render subject line dynamically

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ exports.handler = function(event, context) {
   var When = require('when');
   var Aws = require("aws-sdk");
 
+  var auth = { accessKeyId: 'AKIAJ7T4V2A2LKZNE7SA', secretAccessKey: '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', region: 'us-east-1' };
+
   // load SES and S3 objects for entire of record processing
-  var ses = new Aws.SES();
-  var s3 = new Aws.S3();
+  var ses = new Aws.SES(auth);
+  var s3 = new Aws.S3(auth);
+
+  // // load SES and S3 objects for entire of record processing
+  // var ses = new Aws.SES();
+  // var s3 = new Aws.S3();
 
   // begin processing all the received records..
   var promises = event.Records.map(function(record) {
@@ -20,35 +26,45 @@ exports.handler = function(event, context) {
         return { Bucket: payload.Configuration.Bucket, Key: payload.Email.Properties.TemplateKey + '.' + ext }
       };
 
-      // retrieve the html file from s3
-      s3.getObject(_ext('html'), function(err, html) {
-        if (err) reject(err); else {
-          // OK! now retrieve the txt file from s3
-          s3.getObject(_ext('txt'), function(err, txt) {
-            if (err) reject(err); else {
-              // inject the rendered HTML & TXT messages into the payload to be sent to SES
-              payload.Email.Payload.Message  = {
-                Body: {
-                  Html: {
-                    Data: Mustache.render(html.Body.toString(), payload.Email.Properties.Data),
-                    Charset: 'UTF-8'
-                  },
-                  Text: {
-                    Data: Mustache.render(txt.Body.toString(), payload.Email.Properties.Data),
-                    Charset: 'UTF-8'
-                  }
-                },
+      var _data = function(params, fnc) {
+        s3.headObject(params, function(err, data) {
+          if (err && err.code === 'Not Found') fnc(null); else {
+            s3.getObject(params, function(err, data) {
+              if (err) reject(err); else fnc(data.Body.toString());
+            });
+          }
+        });
+      };
+
+      _data(_ext('subj'), function(subject) {
+        if( ! subject) reject('missing .subj'); else {
+          _data(_ext('html'), function(html) {
+            _data(_ext('txt'), function(txt) {
+              payload.Email.Payload.Message = {
+                Body: {},
                 Subject: {
                   // also set the configured subject
-                  Data: payload.Email.Properties.Subject,
+                  Data: Mustache.render(subject, payload.Email.Properties.Data),
                   Charset: 'UTF-8'
                 }
-              }
+              };
+              // if a html email exists inject it
+              if(html) payload.Email.Payload.Message.Body.Html = {
+                Data: Mustache.render(html, payload.Email.Properties.Data),
+                Charset: 'UTF-8'
+              };
+              // if a text email exists inject it
+              if(txt) payload.Email.Payload.Message.Body.Text = {
+                Data: Mustache.render(txt, payload.Email.Properties.Data),
+                Charset: 'UTF-8'
+              };
+
               // send the email and resolve the promise. Or reject on error
+              console.log(payload.Email.Payload);
               ses.sendEmail(payload.Email.Payload, function (err, data) {
                 if (err) reject(err); else resolve({});
               });
-            }
+            });
           });
         }
       });
